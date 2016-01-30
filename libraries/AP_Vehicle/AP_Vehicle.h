b9bce175651f0ae1b8784f2c61d812eaff76fbfb
Tom Pittenger <magicrub@gmail.com>
2016-01-29 22:38:56 -0800

    AP_Vehicle: new params for pre-flare

--- libraries/AP_Vehicle/AP_Vehicle.h
+++ libraries/AP_Vehicle/AP_Vehicle.h
@@ -40,6 +40,8 @@ public:
         AP_Int8  autotune_level;
         AP_Int16 land_pitch_cd;
         AP_Float land_flare_sec;
+        AP_Float land_pre_flare_alt;
+        AP_Float land_pre_flare_airspeed;
         AP_Int8  stall_prevention;
     };
 
