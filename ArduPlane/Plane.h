548d46244e499d34f00a4088f178379e5d311879
Tom Pittenger <magicrub@gmail.com>
2016-02-11 00:41:04 -0800

    v3.0.2

--- ArduPlane/Plane.h
+++ ArduPlane/Plane.h
@@ -3,7 +3,7 @@
 #ifndef _PLANE_H
 #define _PLANE_H
 
-#define THISFIRMWARE "ArduPlane V3.5.0 - Airphrame v3.0.1"
+#define THISFIRMWARE "ArduPlane V3.5.0 - Airphrame v3.0.2"
 #define FIRMWARE_VERSION 3,5,0,FIRMWARE_VERSION_TYPE_OFFICIAL
 
 /*
