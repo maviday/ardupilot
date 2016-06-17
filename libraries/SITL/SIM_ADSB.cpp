6c7efcc8c1f566e5b397514c5b63c778dd819b40
Tom Pittenger <magicrub@gmail.com>
2016-06-17 12:40:53 -0700

    SITL: add delay for Windows threading bug

--- libraries/SITL/SIM_ADSB.cpp
+++ libraries/SITL/SIM_ADSB.cpp
@@ -109,6 +109,11 @@ void ADSB::update(void)
 */
 void ADSB::send_report(void)
 {
+    if (AP_HAL::millis() < 10000) {
+        // simulated aircraft don't appear until 10s after startup. This avoids a windows
+        // threading issue with non-blocking sockets and the initial wait on uartA
+        return;
+    }
     if (!mavlink.connected && mav_socket.connect(target_address, target_port)) {
         ::printf("ADSB connected to %s:%u\n", target_address, (unsigned)target_port);
         mavlink.connected = true;
