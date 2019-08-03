07011a50d2757242477dbc1011eba90113b8d5e2
Tom Pittenger <tomp@AionRobotics.com>
2019-08-02 17:29:22 -0700

    Revert "DEBUG"
    This reverts commit 0fa696cedfb0e39b41f2a89c73ef79abc78ee78c.

--- APMrover2/mode_acro.cpp
+++ APMrover2/mode_acro.cpp
@@ -3,9 +3,6 @@
 
 void ModeAcro::update()
 {
-    uint32_t now_ms = AP_HAL::millis();
-    static uint32_t gcs_send_last[10] = {};
-
     // get speed forward
     float speed, desired_steering;
     if (!attitude_control.get_forward_speed(speed)) {
@@ -21,14 +18,10 @@ void ModeAcro::update()
             rover.set_brake(rover.g2.attitude_control.get_brake_manual_pct() * attitude_control.get_brake_gain());
         }
     } else {
-        // convert pilot stick input into desired steering and speed
         float desired_speed;
+        // convert pilot stick input into desired steering and speed
         get_pilot_desired_steering_and_speed(desired_steering, desired_speed);
         calc_throttle(desired_speed, true);
-        if (now_ms - gcs_send_last[0] >= 500) {
-            gcs_send_last[0] = now_ms;
-            gcs().send_text(MAV_SEVERITY_DEBUG,"\n1A: desired_steering:%.2f, desired_speed:%.2f", desired_steering, desired_speed);
-        }
     }
 
     float steering_out;
@@ -49,10 +42,7 @@ void ModeAcro::update()
     } else {
         // convert pilot steering input to desired turn rate in radians/sec
         const float target_turn_rate = (desired_steering / 4500.0f) * radians(g2.acro_turn_rate);
-        if (now_ms - gcs_send_last[1] >= 500) {
-            gcs_send_last[1] = now_ms;
-            gcs().send_text(MAV_SEVERITY_DEBUG,"1B: target_turn_rate:%.2f", target_turn_rate);
-        }
+
         // run steering turn rate controller and throttle controller
         steering_out = attitude_control.get_steering_out_rate(target_turn_rate,
                                                               g2.motors.limit.steer_left,
@@ -60,10 +50,6 @@ void ModeAcro::update()
                                                               rover.G_Dt);
     }
 
-    if (now_ms - gcs_send_last[2] >= 500) {
-        gcs_send_last[2] = now_ms;
-        gcs().send_text(MAV_SEVERITY_DEBUG,"1C: steering_out:%.2f", steering_out);
-    }
     set_steering(steering_out * 4500.0f);
 }
 
