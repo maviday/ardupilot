44af6f1b98274573e10bb160f493cae63c4f0e58
Tom Pittenger <tomp@AionRobotics.com>
2019-08-14 10:31:04 -0700

    Revert "Rover: add NAV_DELAY support"
    This reverts commit bda07fc0dbb0db434a40606202990d3e82653913.

--- APMrover2/mode_auto.cpp
+++ APMrover2/mode_auto.cpp
@@ -348,10 +348,6 @@ bool ModeAuto::start_command(const AP_Mission::Mission_Command& cmd)
         do_nav_set_yaw_speed(cmd);
         break;
 
-    case MAV_CMD_NAV_DELAY:                    // 93 Delay the next navigation command
-        do_nav_delay(cmd);
-        break;
-
     // Conditional commands
     case MAV_CMD_CONDITION_DELAY:
         do_wait_delay(cmd);
@@ -476,9 +472,6 @@ bool ModeAuto::verify_command(const AP_Mission::Mission_Command& cmd)
     case MAV_CMD_NAV_GUIDED_ENABLE:
         return verify_nav_guided_enable(cmd);
 
-    case MAV_CMD_NAV_DELAY:
-       return verify_nav_delay(cmd);
-
     case MAV_CMD_CONDITION_DELAY:
         return verify_wait_delay();
 
@@ -545,21 +538,6 @@ bool ModeAuto::do_nav_wp(const AP_Mission::Mission_Command& cmd, bool always_sto
     return true;
 }
 
-// do_nav_delay - Delay the next navigation command
-void ModeAuto::do_nav_delay(const AP_Mission::Mission_Command& cmd)
-{
-    nav_delay_time_start = millis();
-
-    if (cmd.content.nav_delay.seconds > 0) {
-        // relative delay
-        nav_delay_time_max = cmd.content.nav_delay.seconds * 1000; // convert seconds to milliseconds
-    } else {
-        // absolute delay to utc time
-        nav_delay_time_max = AP::rtc().get_time_utc(cmd.content.nav_delay.hour_utc, cmd.content.nav_delay.min_utc, cmd.content.nav_delay.sec_utc, 0);
-    }
-    gcs().send_text(MAV_SEVERITY_INFO, "Delaying %u sec",nav_delay_time_max/1000);
-}
-
 // start guided within auto to allow external navigation system to control vehicle
 void ModeAuto::do_nav_guided_enable(const AP_Mission::Mission_Command& cmd)
 {
@@ -621,22 +599,6 @@ bool ModeAuto::verify_nav_wp(const AP_Mission::Mission_Command& cmd)
     } else {
         return (((millis() - loiter_start_time) / 1000) >= loiter_duration);
     }
-}
-
-// verify_nav_delay - check if we have waited long enough
-bool ModeAuto::verify_nav_delay(const AP_Mission::Mission_Command& cmd)
-{
-    if (millis() - nav_delay_time_start > nav_delay_time_max) {
-        nav_delay_time_max = 0;
-        return true;
-    }
-
-    // if we're a boat, try to loiter. else, stop
-    if (!rover.is_boat() || !start_loiter()) {
-        stop_vehicle();
-    }
-
-    return false;
 }
 
 bool ModeAuto::verify_RTL()
