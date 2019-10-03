0bc97d3a4839067bfcc3459adb0898b40fee7d39
Tom Pittenger <tomp@AionRobotics.com>
2019-10-03 08:45:11 -0700

    Add support for AP_UserCustom library

---
+++ libraries/AP_UserCustom/AP_UserCustom.cpp
@@ -0,0 +1,129 @@
+#include "AP_UserCustom.h"
+
+extern const AP_HAL::HAL& hal;
+
+const AP_Param::GroupInfo AP_UserCustom::var_info[] = {
+        // @Param: ENABLE
+        // @DisplayName: UserCustom Enable/Disable
+        // @Description: UserCustom enable/disable
+        // @User: Standard
+        // @Values: 0:Disabled, 1:Enabled
+        AP_GROUPINFO_FLAGS("ENABLE", 0, AP_UserCustom, _enabled, 0, AP_PARAM_FLAG_ENABLE),
+
+        // @Param: TEST_INT1
+        // @DisplayName: User Custom Integer 1
+        // @Description: User Custom Integer 1
+        // @User: Standard
+        AP_GROUPINFO("TEST_INT1", 1, AP_UserCustom, test_int1, 0),
+
+        // @Param: TEST_INT2
+        // @DisplayName: User Custom Integer 2
+        // @Description: User Custom Integer 3
+        // @User: Standard
+        AP_GROUPINFO("TEST_INT2", 2, AP_UserCustom, test_int2, 0),
+
+        // @Param: TEST_INT3
+        // @DisplayName: User Custom Integer 4
+        // @Description: User Custom Integer 4
+        // @User: Standard
+        AP_GROUPINFO("TEST_INT3", 3, AP_UserCustom, test_int3, 0),
+
+        // @Param: TEST_FLT1
+        // @DisplayName: User Custom Float 1
+        // @Description: User Custom Float 1
+        // @User: Standard
+        AP_GROUPINFO("TEST_FLT1", 4, AP_UserCustom, test_float1, 0),
+
+        // @Param: TEST_FLT2
+        // @DisplayName: User Custom Float 2
+        // @Description: User Custom Float 2
+        // @User: Standard
+        AP_GROUPINFO("TEST_FLT2", 5, AP_UserCustom, test_float2, 0),
+
+        // @Param: TEST_FLT3
+        // @DisplayName: User Custom Float 3
+        // @Description: User Custom Float 3
+        // @User: Standard
+        AP_GROUPINFO("TEST_FLT3", 6, AP_UserCustom, test_float3, 0),
+
+    AP_GROUPEND
+};
+
+void AP_UserCustom::init()
+{
+    // return immediately if not enabled
+    if (!enabled()) {
+        return;
+    }
+
+    // TODO: add custom init work
+
+    is_initialized = true;
+}
+
+// periodic callback that runs at 50Hz
+void AP_UserCustom::update()
+{
+    // return immediately if not enabled
+    if (!enabled()) {
+        return;
+    }
+    if (!is_initialized) {
+        init();
+        return;
+    }
+    const uint32_t now_ms = AP_HAL::millis();
+
+    if (now_ms - last_update_timestamp_ms > 1000) {
+        last_update_timestamp_ms = now_ms;
+        // period work that happens every 1000 ms (1Hz)
+
+        // TODO: add custom periodic slow work
+    }
+
+    // TODO: add custom periodic fast work
+}
+
+// handle inbound MAVLink messages
+bool AP_UserCustom::handle_user_message(const mavlink_command_long_t &packet)
+{
+    if (!enabled()) {
+        return false;
+    }
+
+    switch (packet.command) {
+    case MAV_CMD_WAYPOINT_USER_1:
+    case MAV_CMD_WAYPOINT_USER_2:
+    case MAV_CMD_WAYPOINT_USER_3:
+    case MAV_CMD_WAYPOINT_USER_4:
+    case MAV_CMD_WAYPOINT_USER_5:
+    case MAV_CMD_SPATIAL_USER_1:
+    case MAV_CMD_SPATIAL_USER_2:
+    case MAV_CMD_SPATIAL_USER_3:
+    case MAV_CMD_SPATIAL_USER_4:
+    case MAV_CMD_SPATIAL_USER_5:
+    case MAV_CMD_USER_1:
+    case MAV_CMD_USER_2:
+    case MAV_CMD_USER_3:
+    case MAV_CMD_USER_4:
+    case MAV_CMD_USER_5:
+        // TODO: add custom MAVLink handling work
+        return true;
+
+    default:
+        return false;
+    }
+}
+
+// singleton instance
+AP_UserCustom *AP_UserCustom::_singleton;
+
+namespace AP {
+
+AP_UserCustom *usercustom()
+{
+    return AP_UserCustom::get_singleton();
+}
+
+};
+
---
+++ libraries/AP_UserCustom/AP_UserCustom.h
@@ -0,0 +1,75 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <AP_Param/AP_Param.h>
+#include <GCS_MAVLink/GCS_MAVLink.h>
+
+class AP_UserCustom {
+public:
+    AP_UserCustom() {
+        if (_singleton) {
+    #if CONFIG_HAL_BOARD == HAL_BOARD_SITL
+            AP_HAL::panic("Too many AP_UserCustom");
+    #endif
+            return;
+        }
+        _singleton = this;
+
+        AP_Param::setup_object_defaults(this, var_info);
+    }
+
+    // remove copy constructor for any class that has a singleton
+    AP_UserCustom(const AP_UserCustom &other) = delete;
+    AP_UserCustom &operator=(const AP_UserCustom&) = delete;
+
+    static const struct AP_Param::GroupInfo        var_info[];
+
+    static AP_UserCustom *get_singleton() { return _singleton; }
+
+    // initialise this module
+    void init();
+
+    // update - should be called at at least 10hz but usually 50Hz
+    void update();
+
+    // indicate whether this module is enabled or not
+    bool enabled() const { return _enabled; }
+
+    bool handle_user_message(const mavlink_command_long_t &packet);
+
+    // public parameters
+    AP_Int32    test_int1;
+    AP_Int32    test_int2;
+    AP_Int32    test_int3;
+    AP_Float    test_float1;
+    AP_Float    test_float2;
+    AP_Float    test_float3;
+
+private:
+    static AP_UserCustom *_singleton;
+    bool is_initialized;
+
+    uint32_t last_update_timestamp_ms;
+
+    // private parameters
+    AP_Int8     _enabled;               //  module enable/disable
+
+};
+
+namespace AP {
+    AP_UserCustom *usercustom();
+};
